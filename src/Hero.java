101f3d201547c1142bc104b8105f8036933609b8
Vlad <maptbin1@gmail.com>
2026-03-06 18:15:04 +0300

    Добавил функцию боя через цикл while

--- src/Hero.java
+++ src/Hero.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /*Задание 2. Конструктор Героев (Классы, Объекты, Конструкторы)
 Здесь мы учимся превращать абстрактный код в «живое» существо.
 Задача: Создать класс Hero.
@@ -13,8 +15,8 @@ public class Hero {
     private String name;
     private String heroClass;
     private int level;
-   private double maxHealth;
-   private double remainingHealth;
+    private double maxHealth;
+    private double remainingHealth;
 
     //Создаем конструктор для определения созданных ранее полей
     Hero(String name, String heroClass, int level) {
@@ -23,6 +25,15 @@ public class Hero {
         this.level = level;
         maxHealth = level * 10;
         remainingHealth = maxHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public double getRemainingHealth() {
+        return remainingHealth;
     }
 
     //Создаем метод для получения урона персонажей
